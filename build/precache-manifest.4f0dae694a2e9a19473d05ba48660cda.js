9f59a476a466a33dd87aaffbdc480293b1aa67c3
walid abou ali <walid.abou.ali@ibm.com>
2020-11-18 11:20:03 +0400

    add build

---
+++ build/precache-manifest.4f0dae694a2e9a19473d05ba48660cda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca2258671efc27f603743f847097c70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f97b76ff596bf53586fd",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "92f38242d2701dacf2b3",
+    "url": "/static/js/2.896f3ccf.chunk.js"
+  },
+  {
+    "revision": "822463c87ac05933cda8",
+    "url": "/static/js/3.d1863608.chunk.js"
+  },
+  {
+    "revision": "f97b76ff596bf53586fd",
+    "url": "/static/js/main.8506797e.chunk.js"
+  },
+  {
+    "revision": "0f770bab08dd3dff77fd",
+    "url": "/static/js/runtime~main.d12a0e5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
